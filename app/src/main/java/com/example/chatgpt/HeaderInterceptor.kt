8cc311c500a9625eb6caa91a45300489145b1e45
Anju Kumari <42924677+Anju1415@users.noreply.github.com>
2023-01-31 21:44:02 +0530

    Update HeaderInterceptor.kt

--- app/src/main/java/com/example/chatgpt/HeaderInterceptor.kt
+++ app/src/main/java/com/example/chatgpt/HeaderInterceptor.kt
@@ -9,7 +9,7 @@ class HeaderInterceptor : Interceptor {
         val original: Request = chain.request()
         val request: Request = original.newBuilder()
             //  .method(original.method, original.body)
-            .header("Authorization", "Bearer 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")
+            .header("Authorization", "Bearer API KEY")
             .header("Content-Type", "application/json")
             .build()
         return chain.proceed(request)
